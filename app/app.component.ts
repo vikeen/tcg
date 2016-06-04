be2131298d452f133d0482ec1fb16a07bc0c70d5
John Rake <john.rake12@gmail.com>
2016-06-04 15:40:20 -0500

    Create a player class

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,19 @@
 import {Component} from '@angular/core';
-import {HeaderComponent} from "./components/header/header.component";
-import {GameComponent} from "./components/game/game.component";
-
-interface Player {
-  name:string;
-}
+import {Header} from "./components/header/header.component";
+import {Game} from "./components/game/game.component";
+import {Player} from "./components/player/player.component";
 
 @Component({
   selector: 'tcg-app',
-  directives: [HeaderComponent, GameComponent],
+  directives: [Header, Game],
   templateUrl: "app/app.html"
 })
 export class AppComponent {
   player1: Player;
   player2: Player;
-  
-  constructor() {
-    this.player1 = {
-      name: "George D. Tubs"
-    };
 
-    this.player2 = {
-      name: "Fred Bonk"
-    };
+  constructor() {
+    this.player1 = new Player("George D. Tubs");
+    this.player2 = new Player("Elizabeth Jacobs");
   }
 }
